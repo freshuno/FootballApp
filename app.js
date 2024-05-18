bb663cd4fa2b0b28ff4b1db48b224804fd974509
Biszku <daniel.bivhuniak@gmail.com>
2024-05-18 14:58:02 +0200

    test Api call implementation

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 
 const landingPageRoutes = require('./js/backend/routes/landingPage');
 const errorController = require('./js/backend/controllers/error');
+const getTransfers = require('./js/backend/utils/getTransfers');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -15,6 +16,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(landingPageRoutes);
 app.use(errorController.get404);
+
+const testApi = async() =>{
+  const res = await getTransfers();
+  console.log(res);
+}
+
+testApi();
 
 app.listen(3000, () => {
   console.log(`Server started!`)
